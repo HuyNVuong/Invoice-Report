2e9a5c34aae33ac97c5439971130280dd2815fc5
Nguyen Huy Vuong <vuongnguyenhuy@yahoo.com>
2018-02-15 16:04:24 -0600

    Updated file

---
+++ src/entities/Invoice.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class Invoice {
+	private String invoiceCode;
+	private Members membersCode;
+	private Persons personsCode;
+	private String invoiceDate;
+	private Products productsList;
+}
