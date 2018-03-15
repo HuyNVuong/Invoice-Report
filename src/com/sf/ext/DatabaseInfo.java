23d4251ec60bbdfb516cd283ebd7ba1d4e1327ff
William Kitay <will.kitay@gmail.com>
2018-03-15 11:50:30 -0500

    Updated 11:50am 3/15/18

---
+++ src/com/sf/ext/DatabaseInfo.java
@@ -0,0 +1,43 @@
+package com.sf.ext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseInfo {
+
+	public static final String url = "jdbc:mysql://cse.unl.edu/wkitay";
+	public static final String username = "wkitay";
+	public static final String password = "8P:PrY";
+	
+	static public Connection getConnection()
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
+			System.out.println("InstantiationException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			System.out.println("IllegalAccessException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(DatabaseInfo.url, DatabaseInfo.username, DatabaseInfo.password);
+		} catch (SQLException e) {
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		return conn;
+	}
+}
