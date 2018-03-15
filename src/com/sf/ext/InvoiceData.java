6054895e18376b92d8da450e12ab5a74c98497b6
William Kitay <will.kitay@gmail.com>
2018-03-15 11:14:57 -0500

    Updated 11:14AM 3/15/18

---
+++ src/com/sf/ext/InvoiceData.java
@@ -0,0 +1,173 @@
+package com.sf.ext;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+/*
+ * This is a collection of utility methods that define a general API for
+ * interacting with the database supporting this application.
+ * 16 methods in total, add more if required.
+ * Do not change any method signatures or the package name.
+ * 
+ */
+
+//import com.sf.model.DayMembership;
+//import com.sf.model.Invoice;
+//import com.sf.model.Member;
+//import com.sf.model.Membership;
+//import com.sf.model.Parkingpass;
+//import com.sf.model.Person;
+//import com.sf.model.Product;
+//import com.sf.model.RentalEquipment;
+//import com.sf.model.YearLongMembership;
+
+public class InvoiceData {
+
+	/**
+	 * 1. Method that removes every person record from the database
+	 */
+	public static void removeAllPersons() {
+		/** TODO*/
+	}
+
+	/**
+	 * 2. Method to add a person record to the database with the provided data.
+	 * 
+	 * @param personCode
+	 * @param firstName
+	 * @param lastName
+	 * @param street
+	 * @param city
+	 * @param state
+	 * @param zip
+	 * @param country
+	 */
+	public static void addPerson(String personCode, String firstName, String lastName, String street, String city, String state, String zip, String country) {
+		/** TODO*/
+	}
+
+	/**
+	 * 3. Adds an email record corresponding person record corresponding to the
+	 * provided <code>personCode</code>
+	 * 
+	 * @param personCode
+	 * @param email
+	 */
+	public static void addEmail(String personCode, String email) {
+		/** TODO*/
+	}
+
+	/**
+	 * 4. Method that removes every member record from the database
+	 */
+	public static void removeAllMembers() {
+		/** TODO*/
+	}
+	/**
+	 * 5. Method to add a member record to the database with the provided data
+	 * @param memberCode
+	 * @param memberType
+	 * @param primaryContactPersonCode
+	 * @param name
+	 * @param street
+	 * @param city
+	 * @param state
+	 * @param zip
+	 * @param country
+	 */
+	public static void addMember(String memberCode, String memberType, String primaryContactPersonCode,String name, String street, String city, String state, String zip, String country) {
+		/** TODO*/
+	}
+	
+	/**
+	 * 6. Removes all product records from the database
+	 */
+	public static void removeAllProducts() {
+		/** TODO*/
+	}
+
+	/**
+	 * 7. Adds a day-pass record to the database with the provided data.
+	 */
+	public static void addDayPass(String productCode, String dateTime, String street, String city, String state, String zip, String country, double pricePerUnit) {
+		/** TODO*/
+	}
+
+	/**
+	 * 8. Adds a year-long-pass record to the database with the provided data.
+	 */
+	public static void addYearPass(String productCode, String StartDate, String EndDate,String street, String city, String state, String zip, String country, String name, double pricePerUnit) {
+		/** TODO*/
+	}
+
+	/**
+	 * 9. Adds a ParkingPass record to the database with the provided data.
+	 */
+	public static void addParkingPass(String productCode, double parkingFee) {
+        /** TODO*/
+	}
+
+	/**
+	 * 10. Adds an equipment rental record to the database with the provided data.
+	 */
+	public static void addRental(String productCode, String name, double cost) {
+        /** TODO*/
+	}
+
+	/**
+	 * 11. Removes all invoice records from the database
+	 */
+	public static void removeAllInvoices() {
+        /** TODO*/
+	}
+
+	/**
+	 * 12. Adds an invoice record to the database with the given data.
+	 */
+	public static void addInvoice(String invoiceCode, String memberCode, String personalTrainerCode, String invoiceDate) {
+		/** TODO*/
+	}
+
+	/**
+	 * 13. Adds a particular day-pass (corresponding to <code>productCode</code>
+	 * to an invoice corresponding to the provided <code>invoiceCode</code> with
+	 * the given number of units
+	 */
+
+	public static void addDayPassToInvoice(String invoiceCode, String productCode, int quantity) {
+		/** TODO*/
+	}
+
+	/**
+	 * 14. Adds a particular year-long-pass (corresponding to <code>productCode</code>
+	 * to an invoice corresponding to the provided <code>invoiceCode</code> with
+	 * the given begin/end dates
+	 */
+	public static void addYearPassToInvoice(String invoiceCode, String productCode, int quantity) {
+		/** TODO*/
+	}
+
+     /**
+     * 15. Adds a particular ParkingPass (corresponding to <code>productCode</code> to an 
+     * invoice corresponding to the provided <code>invoiceCode</code> with the given
+     * number of quantity.
+     * NOTE: membershipCode may be null
+     */
+    public static void addParkingPassToInvoice(String invoiceCode, String productCode, int quantity, String membershipCode) {
+    	/** TODO*/
+    }
+	
+    /**
+     * 16. Adds a particular equipment rental (corresponding to <code>productCode</code> to an 
+     * invoice corresponding to the provided <code>invoiceCode</code> with the given
+     * number of quantity. 
+     * NOTE: membershipCode may be null
+     */
+    public static void addRentalToInvoice(String invoiceCode, String productCode, int quantity, String membershipCode) {
+    	/** TODO*/
+    }
+
+   
+}
--- src/entities/ParkingPasses.java
+++ src/entities/ParkingPasses.java
@@ -9,17 +9,19 @@ public class ParkingPasses extends Service {
 	private String membersType;
 	private int quantity;
 	private String attach;
-	
-	
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 	public String getAttach() {
 		return attach;
 	}
+
 	public void setAttach(String attach) {
 		this.attach = attach;
 	}
@@ -32,35 +34,36 @@ public class ParkingPasses extends Service {
 		this.parkingFee = parkingFee;
 	}
 
-
 	public String getMembersType(Members members) {
 		membersType = members.getType();
 		return membersType;
 	}
-	
-	
+
 	public boolean isStudent() {
 		this.isStudent = membersType.toLowerCase().equals("s");
 		return isStudent;
 	}
+
 	public void setStudent(boolean isStudent) {
 		this.isStudent = membersType.toLowerCase().equals("s");
 		this.isStudent = isStudent;
 	}
+
 	public ParkingPasses(String productsCode, String productsType, String parkingFee) {
 		super(productsCode, productsType);
 		this.parkingFee = parkingFee;
 		this.attach = null;
 		this.quantity = 0;
 	}
-	
+
 	public ParkingPasses(ParkingPasses service, int quantity, String attach) {
 		super(service, quantity, attach);
 		this.parkingFee = service.parkingFee;
 		this.quantity = service.getProductsQuantity();
 		this.attach = service.getProductsCodeAttach();
-		 
+
 	}
+
 	public ParkingPasses(ParkingPasses service, int quantity) {
 		super(service, quantity);
 		this.parkingFee = service.parkingFee;
@@ -77,8 +80,8 @@ public class ParkingPasses extends Service {
 	public double getTax() {
 
 		double tax = 0.0;
-		
-		if(!isStudent) {
+
+		if (!isStudent) {
 			tax = this.getCost() * 0.04;
 		}
 		return tax;
