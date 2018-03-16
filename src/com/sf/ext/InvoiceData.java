5250664d7621c9810b132f01884eb2f2a0b857b6
William Kitay <will.kitay@gmail.com>
2018-03-16 01:47:59 -0500

    Updated 1:47am 3/16/18

--- src/com/sf/ext/InvoiceData.java
+++ src/com/sf/ext/InvoiceData.java
@@ -5,6 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
+import entities.Persons;
+import entities.PersonsAddress;
 /*
  * This is a collection of utility methods that define a general API for
  * interacting with the database supporting this application.
@@ -29,13 +32,13 @@ public class InvoiceData {
 	 * 1. Method that removes every person record from the database
 	 */
 	public static void removeAllPersons() {
+		String update = "TRUNCATE TABLE Persons";
 
 		Connection conn = DatabaseInfo.getConnection();
-		String update = "TRUNCATE TABLE Persons";
+
 		try {
 			PreparedStatement ps = conn.prepareStatement(update);
 			ps.executeUpdate();
-			
 			ps.close();
 			conn.close();
 		} catch (SQLException e) {
@@ -59,7 +62,108 @@ public class InvoiceData {
 	 */
 	public static void addPerson(String personCode, String firstName, String lastName, String street, String city,
 			String state, String zip, String country) {
-		/** TODO */
+
+		// FIXME how to test?
+
+		// FIXME does my address/person check actually work/make sense? Is there a better way to do it?
+		int foreignKeyID;
+		String checkAddressQuery = "SELECT ?, ?, ?, ?, ? FROM Address";
+		Connection conn1 = DatabaseInfo.getConnection();
+		try {
+			PreparedStatement ps = conn1.prepareStatement(checkAddressQuery);
+			ps.setString(1, street);
+			ps.setString(2, city);
+			ps.setString(3, state);
+			ps.setString(4, zip);
+			ps.setString(5, country);
+			ResultSet rs = ps.executeQuery();
+			ps.close();
+			while (rs.next()) {
+				if (rs.getString("Street").equalsIgnoreCase(street) && rs.getString("City").equalsIgnoreCase(city)
+						&& rs.getString("State").equalsIgnoreCase(state) && rs.getString("Zip").equalsIgnoreCase(zip)
+						&& rs.getString("Country").equalsIgnoreCase(country)) {
+					// FIXME prevent address insertion
+				} else {
+					String insertAddressQuery = "INSERT INTO Address (Street, City, State, Zip, Country) VALUES (?,?,?,?,?);";
+					Connection conn2 = DatabaseInfo.getConnection();
+					try {
+						PreparedStatement ps2 = conn2.prepareStatement(insertAddressQuery);
+						ps2.setString(1, street);
+						ps2.setString(2, city);
+						ps2.setString(3, state);
+						ps2.setString(4, zip);
+						ps2.setString(5, country);
+						ps2.executeUpdate();
+						ps2.close();
+						
+						ps = conn2.prepareStatement("SELECT LAST_INSERT_ID()");
+						ResultSet rs2 = ps.executeQuery();
+						rs2.next();
+						foreignKeyID = rs2.getInt("LAST_INSERT_ID()");
+						conn2.close();
+						rs2.close();
+
+					} catch (SQLException e) {
+						System.out.println("SQLException: ");
+						e.printStackTrace();
+						throw new RuntimeException(e);
+					}
+				}
+			}
+			rs.close(); // FIXME CAN I HAVE A CONN/PS/RS inside of another conn/ps/rs?
+			conn1.close();
+		} catch (SQLException e) {
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		String checkPersonQuery = "SELECT ?,?,? FROM Persons";
+		Connection conn4 = DatabaseInfo.getConnection();
+		try {
+		PreparedStatement ps = conn4.prepareStatement(checkPersonQuery);
+		ps.setInt(1, Integer.parseInt(personCode));
+		ps.setString(2, firstName);
+		ps.setString(3, lastName);
+		ResultSet rs = ps.executeQuery();
+		
+		while (rs.next()) {
+			if (rs.getInt("PersonCode") == Integer.parseInt(personCode) && rs.getString("PersonFirstName").equalsIgnoreCase(firstName) 
+					&& rs.getString("PersonLastName").equalsIgnoreCase(lastName)) {
+				// FIXME prevent Person insertion below
+			}
+			else {
+				String insertPersonQuery = "INSERT INTO Persons (PersonCode, PersonFirstName, PersonLastName) VALUES (?,?,?) WHERE AddressID = ?;";
+				Connection conn3 = DatabaseInfo.getConnection();
+				try {
+					// FIXME check if student already exists? --> SELECT statement to retrieve
+					// AddressID from Address table?
+					PreparedStatement ps1 = conn3.prepareStatement(insertPersonQuery);
+					ps1.setInt(1, Integer.parseInt(personCode));
+					ps1.setString(2, firstName);
+					ps1.setString(3, lastName);
+					//ps1.setInt(4, foreignKeyID); FIXME can't access foreignKeyID
+					ps1.executeUpdate();
+					ps1.close();
+					conn3.close();
+
+				} catch (SQLException e) {
+					System.out.println("SQLException: ");
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		rs.close(); //FIXME conn inside conn
+		ps.close();
+		conn4.close();
+		
+		} catch (SQLException e) {
+			System.out.println("SQLException: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
 	}
 
 	/**
@@ -188,9 +292,9 @@ public class InvoiceData {
 	public static void addRentalToInvoice(String invoiceCode, String productCode, int quantity, String membershipCode) {
 		/** TODO */
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hello");
+
 	}
 
 }
