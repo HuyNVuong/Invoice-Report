e6b52b9d6582d37fbedd46f40c11b2d905baf14a
William Kitay <will.kitay@gmail.com>
2018-04-04 12:05:46 -0500

    updated 12:05

--- src/adt/InvoiceNode.java
+++ src/adt/InvoiceNode.java
@@ -1,5 +1,32 @@
 package adt;
 
+import entities.Invoice;
+
 public class InvoiceNode<T> {
+
+	private InvoiceNode<T> next;
+	private Invoice item;
+
+	public InvoiceNode(Invoice item) {
+		super();
+		this.item = item;
+		this.next = null;
+	}
+
+	public InvoiceNode<T> getNext() {
+		return next;
+	}
+
+	public void setNext(InvoiceNode<T> next) {
+		this.next = next;
+	}
+
+	public Invoice getItem() {
+		return item;
+	}
+
+	public void setItem(Invoice item) {
+		this.item = item;
+	}
 
 }
---
+++ src/adt/TotalComparator.java
@@ -0,0 +1,14 @@
+package adt;
+
+import java.util.Comparator;
+import entities.Invoice;
+
+public class TotalComparator implements Comparator<Invoice>{
+
+	@Override
+	public int compare(Invoice inv1, Invoice inv2) {
+		
+		return 0;
+	}
+	
+}
